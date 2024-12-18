ba1b2249f02045f6aa266c06dcbdd8e65df008aa
Sivasothy-Tharsa <tharsatharsi2000@gmail.com>
2024-12-18 16:54:38 +0530

    Image upload integrated with cloud

--- start/src/components/Product.tsx
+++ start/src/components/Product.tsx
@@ -1,7 +1,100 @@
+'use client';
 import { Button } from "@nextui-org/button";
 import Link from "next/link";
+import {handleFormSubmit} from '../action/action'
+import { useState } from "react";
+import { Oval } from "react-loader-spinner";
 
 export default function Product() {
+    const [isUploading, setIsUploading] = useState(false)
+    const [fileName, setFileName] = useState('')
+
+    const [productData, setProductData] = useState({
+        title: '',
+        category: '',
+        price: '',
+        image: '',
+        description: '',
+      });
+
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setProductData((prev)=>({...prev, [name]:value}))
+      };
+
+    const handleFileChange = async (e) => {
+        const file = e.target.files[0];
+        if (file) {
+          setIsUploading(true);
+          try {
+            const formData = new FormData();
+            formData.append('my-file', file);
+            const result = await handleFormSubmit(productData, formData); // Ensure the correct data is passed
+            if (result.status === 'success') {
+              console.log("result",result)
+              console.log("uploaded path :", result?.path)
+              setFileName(result.fileName)
+              console.log(result.fileName)
+              setProductData({ ...productData, image: result?.path });
+              setIsUploading(false);
+            } else {
+              alert('Error uploading image: ' + result.message);
+            }
+          } catch (error) {
+            console.error('Error uploading image:', error);
+            alert('Error uploading image');
+          } finally {
+            setIsUploading(false)
+          }
+        }
+      };
+
+
+      
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log("Submitting product:", productData); 
+
+    const formData = new FormData();
+    formData.append('name', productData.title);
+    formData.append('category', productData.category);
+    formData.append('price', productData.price);
+    formData.append('description', productData.description);
+  
+    if (productData.image) {
+      formData.append('image', productData.image);
+    } else {
+      alert('Please upload an image');
+      return;
+    }
+
+    console.log("1", formData)
+
+    for (let pair of formData.entries()) {
+      console.log(pair[0] + ': ' + pair[1]);
+    }
+
+    
+    // try {
+    //   console.log("first", formData)
+    //   const res = await fetch('http://localhost:3000/api/product', {
+    //     method: 'POST',
+    //     body: formData,
+    //   });
+
+    //   const data = await res.json();
+    //   if (res.ok) {
+    //     alert('Product added successfully!');
+    //   } else {
+    //     console.error('Error adding product:', data.error);
+    //     alert('Error adding product: ' + data.error);
+    //   }
+    // } catch (error) {
+    //   console.error('Error during fetch:', error);
+    //   alert('An error occurred while adding the product.');
+    // }
+  };
     return (
         <>
         <div className="lg:mx-auto max-w-screen-lg sm:mx-10 md:mx-16">
@@ -16,6 +109,9 @@ export default function Product() {
                 <input
                     type="text"
                     id="example1"
+                    name="title"
+                    value={productData.title}
+                    onChange={handleChange}
                     className="bg-white text-gray-800 border p-3 block w-full rounded-md border-purple-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-black disabled:text-black"
                     placeholder="Product Title"
                 />
@@ -32,6 +128,9 @@ export default function Product() {
                 </label>
                 <select
                     className="bg-white text-gray-800 border p-3 block w-full rounded-md border-purple-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-black disabled:text-black"
+                    name="category"
+                    value={productData.category}
+                    onChange={handleChange}
                 >
                     <option value="">Mobile Phone</option>
                     <option value="">Laptop</option>
@@ -58,7 +157,7 @@ export default function Product() {
                         <div className="space-y-1 text-center">
                         {/* Upload Icon */}
                             <div className="mx-auto inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-100">
-                                <svg
+                                {!isUploading && <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
                                 viewBox="0 0 24 24"
@@ -71,9 +170,27 @@ export default function Product() {
                                     strokeLinejoin="round"
                                     d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
                                 />
-                                </svg>
+                                </svg>}
+
+                                {isUploading && (
+                                    <Oval
+                                      height={30}
+                                      width={30}
+                                      color="#4fa94d"
+                                      wrapperStyle={{}}
+                                      wrapperClass="loader"
+                                      visible={true}
+                                      ariaLabel="oval-loading"
+                                      secondaryColor="#4fa94d"
+                                      strokeWidth={2}
+                                      strokeWidthSecondary={2}
+                                    />
+                                  )}
                             </div>
 
+                            {fileName && fileName.length > 0 &&(
+                                <p>{fileName}</p>
+                            )}
                             {/* Upload Instructions */}
                             <div className="text-gray-600">
                                 <a
@@ -92,7 +209,12 @@ export default function Product() {
                             </div>
 
                             {/* Hidden file input */}
-                            <input id="example5" type="file" className="sr-only" />
+                            <input 
+                                id="example5" 
+                                type="file" 
+                                name="image" 
+                                className="sr-only" 
+                                onChange={handleFileChange} accept="image/**" required />
                             </label>
                         </div>
                     </div>
@@ -107,6 +229,9 @@ export default function Product() {
                 </label>
                 <textarea
                     id="example1"
+                    name="description"
+                    value={productData.description}
+                    onChange={handleChange}
                     rows={5}
                     className="bg-white text-gray-800 border p-3 block w-full rounded-md border-purple-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-black disabled:text-black"
                     placeholder="Add a description of the product"
@@ -124,6 +249,9 @@ export default function Product() {
                 <input
                     type="number"
                     id="example1"
+                    name="price"
+                    value={productData.price}
+                    onChange={handleChange}
                     className="bg-white text-gray-800 border p-3 block w-full rounded-md border-purple-300 shadow-sm focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-black disabled:text-black"
                     placeholder="Product Price"
                 />
@@ -142,3 +270,5 @@ export default function Product() {
         </>
     );
 }
+
+
