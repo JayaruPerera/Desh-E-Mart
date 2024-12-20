6dff1a5190f7dab4fe3f76c13e6a5b46c62c5ae5
Sivasothy-Tharsa <tharsatharsi2000@gmail.com>
2024-12-20 11:23:05 +0530

    Screen responsiveness changed

--- start/src/app/admin/dashboard/productList/page.tsx
+++ start/src/app/admin/dashboard/productList/page.tsx
@@ -40,7 +40,7 @@ export default function ProductList() {
             {loading ? (
                 <div className="text-center text-white">Loading...</div>
             ) : (
-                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4 px-4 mx-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4 mx-4">
                     {products.map((product) => (
                         <div
                             key={product._id}
