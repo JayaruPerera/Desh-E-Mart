48d8c4cc31bfe994d11cbdfab42d050b9c7b74c4
Sathursanak <132430609+Sathursanak@users.noreply.github.com>
2025-01-08 17:03:13 +0530

    Update cctv page

--- start/src/app/products/cctv/page.tsx
+++ start/src/app/products/cctv/page.tsx
@@ -1,5 +1,15 @@
+import HeroSection from "../hero";
+import HeroCCTVImage from "@/assets/images/pcctv.hero.png"
+
 function CCTVDetails() {
-    return ( "CCTV details" );
+    return ( 
+        <div>
+            <HeroSection heroImage={HeroCCTVImage.src}/>
+            
+            <h1>CCTV Details</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel neque id ex consequat malesuada.</p>
+        </div>
+    );
 }
 
 export default CCTVDetails;
