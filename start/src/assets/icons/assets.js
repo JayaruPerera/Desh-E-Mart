d8b452bc53c244374fc0cbaae30826baf71e81e7
Tharindu-Lakmal <126395249+Tharindu-Lakmal@users.noreply.github.com>
2025-02-21 15:02:49 +0530

    Home page added

---
+++ start/src/assets/icons/assets.js
@@ -0,0 +1,45 @@
+import service from './services.png';
+import hours from './hours.png';
+import team from './team.png';
+import repair1 from './repair1.png';
+import repair2 from './repair2.png';
+import repair3 from './repair3.png';
+import battery from './battery.png';
+import screen from './screen.png';
+import water from './water.png';
+import charging from './charging.png';
+import speaker from './speaker.png';
+import os from './os.png';
+import at from './at.png';
+import route from './route.png';
+import offer from './offer.png';
+import facebook from './facebook.png';
+import instagram from './instagram.png';
+import tiktok from './tiktok.png';
+import address from './address.png';
+import email from './email.png';
+import phone from './phone.png';
+
+export const icons = {
+    service,
+    hours,
+    team,
+    repair1,
+    repair2,
+    repair3,
+    battery,
+    screen,
+    water,
+    charging,
+    speaker,
+    os,
+    at,
+    route,
+    offer,
+    facebook,
+    instagram,
+    tiktok,
+    address,
+    email,
+    phone
+}
